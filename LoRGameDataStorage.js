c2fcabedf0187eee6f7c1fbd8b407c03f000ab7e
Sitja <sitja326@hotmail.fr>
2020-01-26 21:34:23 +0100

    Storing logs

--- LoRGameDataStorage.js
+++ LoRGameDataStorage.js
@@ -36,15 +36,15 @@ function trackGame() {
         // If there is no active deck, relaunch trackGame after 5seconds
         else {
             wait(1500);
-            console.log("No active deck found, relaunching tracker...");
+            log("No active deck found, relaunching tracker...");
             trackGame();
         }
     });
 }
 
 function storeDeck(deckAsJson) {
-    console.log('Called static-decklist and returned ' + JSON.stringify(deckAsJson));
-    createFileAndAppend('deck', JSON.stringify(deckAsJson));
+    log('Called static-decklist and returned ' + JSON.stringify(deckAsJson));
+    createFileAndWrite('deck', JSON.stringify(deckAsJson));
 }
 
 async function getDeck() {
@@ -70,9 +70,9 @@ function storeData() {
             wait(2000);
             storeData();
         } else {
-            console.log("Game has ended !");
+            log("Game has ended !");
             getGameResult().then( json => {
-                createFileAndAppend('Game Result', JSON.stringify(json));
+                createFileAndWrite('Game Result', JSON.stringify(json));
             });
             trackGameState();
         }
@@ -84,10 +84,10 @@ async function isPlayerInGame() {
 
     // Checks if there is an active deck
     if (json.GameState !== "InProgress") {
-        console.log("Player is not in game");
+        log("Player is not in game");
         return false;
     } else {
-        console.log("Game in progress");
+        log("Game in progress");
         return true;
     }
 }
@@ -96,14 +96,14 @@ async function getCardPositions() {
     const cardPositionsAsJson = await getLoRData('positional-rectangles');
     const cardPositionsAsString = JSON.stringify(cardPositionsAsJson);
     if (cardPositionsAsString !== previousCardPositions) {
-        console.log('Called positional-rectangles and returned ' + cardPositionsAsString);
+        log('Called positional-rectangles and returned ' + cardPositionsAsString);
     }
     return cardPositionsAsJson;
 }
 
 async function getGameResult() {
     const gameResultAsJson = await getLoRData('game-result');
-    console.log('Called game-result and returned ' + JSON.stringify(gameResultAsJson));
+    log('Called game-result and returned ' + JSON.stringify(gameResultAsJson));
     return gameResultAsJson;
 }
 
@@ -114,17 +114,15 @@ async function getLoRData(endpoint) {
     try {
         const response = await fetch(baseUrl);
         const json = await response.json();
-        console.log('Called endpoint ' + endpoint);
+        log('Called endpoint ' + endpoint);
         return json
     } catch (error) {
-        console.log(error);
+        log(error);
     }
 }
 
-function createFileAndAppend(fileName, content) {
-    fs.appendFile(directory + '/'+ fileName, content, function (error) {
-        if (error) throw error;
-    });
+function createFileAndAppend(filePath, content) {
+    fs.appendFile(filePath, content, function (error) { if (error) { log(error); throw error;  } });
 }
 
 function createFileAndWrite(fileName, content) {
@@ -149,10 +147,17 @@ function createDirectory(path) {
     fs.mkdirSync(path, { recursive: true })
 }
 
-function wait(ms){
+function wait(ms) {
     const start = new Date().getTime();
     let end = start;
     while(end < start + ms) {
         end = new Date().getTime();
     }
+}
+
+function log(message) {
+    const today = new Date();
+    const log = getTime(today) + ' ' + message;
+    console.log(log);
+    createFileAndAppend('logs.txt', '\n' + log);
 }
